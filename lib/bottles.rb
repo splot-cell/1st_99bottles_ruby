3c64babaef03bd42e61868f1430a94d27961c052
Olly Seber <olly_s@hotmail.co.uk>
2022-09-13 21:38:37 -0700

    Implement BottleNumber.to_s

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,9 +12,9 @@ class Bottles
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
-    "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+    "#{bottle_number} of beer.\n" +
     "#{bottle_number.action}, " +
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{next_bottle_number} of beer on the wall.\n"
   end
 end
 
@@ -62,5 +62,9 @@ class BottleNumber
     else
       number - 1
     end
+  end
+
+  def to_s
+    "#{quantity} #{container}"
   end
 end
